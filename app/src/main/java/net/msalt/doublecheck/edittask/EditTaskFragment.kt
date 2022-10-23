6dd60991ff7de48f037f254074174937b9177353
msaltnet <salt.jeong@gmail.com>
2022-10-23 22:12:30 +0900

    add EditTaskViewModel

--- app/src/main/java/net/msalt/doublecheck/edittask/EditTaskFragment.kt
+++ app/src/main/java/net/msalt/doublecheck/edittask/EditTaskFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import net.msalt.doublecheck.R
 import net.msalt.doublecheck.databinding.EditTaskFragBinding
@@ -13,6 +14,8 @@ import net.msalt.doublecheck.databinding.EditTaskFragBinding
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class EditTaskFragment : Fragment() {
+
+    private val viewModel by viewModels<EditTaskViewModel>()
 
     private var _binding: EditTaskFragBinding? = null
 
@@ -26,8 +29,8 @@ class EditTaskFragment : Fragment() {
     ): View? {
 
         _binding = EditTaskFragBinding.inflate(inflater, container, false)
+        binding.viewmodel = viewModel
         return binding.root
-
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/net/msalt/doublecheck/edittask/EditTaskViewModel.kt
@@ -0,0 +1,9 @@
+package net.msalt.doublecheck.edittask
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class EditTaskViewModel : ViewModel() {
+
+    var title = "string"
+}
