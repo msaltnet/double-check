d05a3954e41cf6aecd62ebc8077f971ab0bf821a
msaltnet <salt.jeong@gmail.com>
2022-11-08 01:11:28 +0900

    add room dependency

--- app/src/main/java/net/msalt/doublecheck/edittask/EditTaskFragment.kt
+++ app/src/main/java/net/msalt/doublecheck/edittask/EditTaskFragment.kt
@@ -2,20 +2,14 @@ package net.msalt.doublecheck.edittask
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.MutableLiveData
-import androidx.navigation.fragment.findNavController
-import net.msalt.doublecheck.R
 import net.msalt.doublecheck.data.CheckItem
 import net.msalt.doublecheck.databinding.EditTaskFragBinding
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
 class EditTaskFragment : Fragment() {
 
     private val viewModel by viewModels<EditTaskViewModel>()
